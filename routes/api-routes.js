598f1c512428e96541ad6b40455aad5943352852
Mao Lin <lin.maoxian0991@gmail.com>
2019-11-21 14:07:10 -0500

    Added a few post routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -19,4 +19,37 @@ module.exports = function(app) {
           res.json(dbufo);
         });
     });
+
+    // Post route for user sign up
+    app.post("/api/signup", function(req, res) {
+      db.User.create(req.body).then(function(dbUser) {
+        res.json(dbUser);
+      });
+    });
+
+    // Post route for loging a new sighting
+    app.post("/api/sighting/log", function(req, res) {
+      db.ufo.create(req.body).then(function(dbUfo) {
+        res.json(dbUfo);
+      });
+    });
+
+    // Post route for like/dislike updating for log.
+    app.post("/api/sighting/log/:rating", function(req, res) {
+      var rating = {
+        
+        }
+      db.ufo.update(
+        {
+          rating : 
+        },
+        {
+          where: {
+            id: 
+          }
+        },
+        ).then(function(dbAuthor) {
+        res.json(dbAuthor);
+      });
+    });
 };

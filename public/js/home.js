3bfcf166c34211f62a22189eed56668dd07d2b62
Mao <lin.maoxian0991@gmail.com>
2019-12-06 03:48:00 -0500

    changed the zoom of the google map

--- public/js/home.js
+++ public/js/home.js
@@ -6,7 +6,7 @@ function initMap() {
             lat: 41.4993,
             lng: -81.6944
         },
-        zoom: 1,
+        zoom: 2,
         styles: [{
                 elementType: 'geometry',
                 stylers: [{
